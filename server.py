0dd2a4875a2beb1afd2f1e004f4c66e083e98c6e
yuhaoda <yhd9487@gmail.com>
2023-04-27 04:34:12 -0400

    解决了server.py的一些小毛病

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import socket
 
-HOST = '192.168.1.14'
+HOST = '192.168.1.42'
+# HOST = '10.140.120.211'
 PORT = 9587
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -14,20 +15,27 @@ while True:
     print(f"[+] received a connection from -> {addr}")
 
     while True:
-        file_info = conn.recv(1048576).decode().replace("\r\n", "|").replace("\n", "|").strip().split("|")
-        print(file_info)
-        print(file_info[0], int(file_info[1]))
-        file_name, file_size = file_info[0], int(file_info[1])
-        print(f"Received file {file_name} ({file_size} bytes)")
+        file_info = conn.recv(1048576).decode()
+        if file_info == 'over':
+            print("\nFile receive complete")
+            print("Connection closed")
+            break
+        else:
+            file_info = file_info.replace("\r\n", "|").replace("\n", "|").strip().split("|")
+            print(f"\n", file_info)
+            print(file_info[0], int(file_info[1]))
+            file_name, file_size = file_info[0], int(file_info[1])
+            print(f"Received file {file_name} ({file_size} bytes)")
 
-        with open(file_name, "wb") as f:
-            remaining_size = file_size
-            while remaining_size > 0:
-                data_size = min(1048576, remaining_size)
-                data = conn.recv(data_size)
-                print(data)
-                f.write(data)
-                remaining_size -= len(data)
-                print(remaining_size)
+            with open(file_name, "wb") as f:
+                remaining_size = file_size
+                while remaining_size > 0:
+                    data_size = min(1048576, remaining_size)
+                    data = conn.recv(data_size)
+                    print(data)
+                    f.write(data)
+                    remaining_size -= len(data)
+                    print(f"Remaining Size:", remaining_size)
 
-        print("File saved successfully")
+            print("File saved successfully")
+    break
